d84ad784874f6d1aa00789cc45d7371f39f21bf5
= <=>
2021-05-03 00:29:13 +0545

    code:routes linked to the main index

--- server.js
+++ server.js
@@ -4,5 +4,18 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/api/notes',require('./routes/api/notes'));
+
+app.get('/notes',(req,res) => {
+    res.sendFile(path.join(__dirname,'public/notes.html'));
+});
+
+app.get('*',(req,res) => {
+    res.sendFile(path.join(__dirname,'public/notes.html'));
+});
 
 app.listen(port,()=> console.log(`Listening on port ${port}...`));
